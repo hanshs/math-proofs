31d54c3700a6ef2eaf94192d8fd71d0a13c9492a
hanshs <hansh.starkopf@gmail.com>
2021-11-07 21:55:49 +0200

    Delete sections when creating a theorem (#41)

--- components/CreateProofSteps.tsx
+++ components/CreateProofSteps.tsx
@@ -10,11 +10,14 @@ export interface IProofStepCreate {
 interface CreateProofStepsProps {
     onChange: (steps: IProofStepCreate[]) => void
     className?: string
+    isSubProof?: boolean
+    onRemoveSubProof?: () => void
 }
 
 export function CreateProofSteps(props: CreateProofStepsProps) {
     const emptyClaim = { claim: { statement: '' } }
     const [steps, setSteps] = React.useState<IProofStepCreate[]>([emptyClaim])
+    const [hoverStep, setHoverStep] = React.useState<number | undefined>()
 
     const addStep = () => {
         const newSteps = [...steps]
@@ -40,6 +43,17 @@ export function CreateProofSteps(props: CreateProofStepsProps) {
         setSteps(newSteps)
     }
 
+    const removeStep = (stepIndex: number) => {
+        const newSteps = [...steps]
+        newSteps.splice(stepIndex, 1)
+        setSteps(newSteps)
+    }
+
+    const removeSubProof = (stepIndex: number) => {
+        const newSteps = steps.map((step, index) => index === stepIndex ? { ...step, subProof: undefined } : step)
+        setSteps(newSteps)
+    }
+
     React.useEffect(() => {
         props.onChange(steps)
         // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -49,7 +63,7 @@ export function CreateProofSteps(props: CreateProofStepsProps) {
         <ol className="space-y-4">
             {steps.map((step, stepIndex) => (
 
-                <li className="space-x-4 flex" key={stepIndex}>
+                <li className={`space-x-4 flex justify-between p-2 rounded ${stepIndex === hoverStep ? ' bg-red-200' : ''}`} key={stepIndex}>
                     <div className="flex items-center">
 
                         <div className="text-2xl font-semibold mr-4 self-start pt-2">{stepIndex + 1}.</div>
@@ -59,19 +73,25 @@ export function CreateProofSteps(props: CreateProofStepsProps) {
                             <CreateClaim claim={step.claim} onChange={(claim) => updateClaim(claim, stepIndex)} />
 
                             {step.subProof && (
-                                <div className="text-sm scale-95 p-3 border">
-                                    <CreateProofSteps onChange={(subProof) => updateSubProof(subProof, stepIndex)} />
+                                <div className={`text-sm scale-95 p-3 border w-full`}>
+                                    <CreateProofSteps onChange={(subProof) => updateSubProof(subProof, stepIndex)} isSubProof={true} onRemoveSubProof={() => removeSubProof(stepIndex)} />
                                 </div>
                             )}
                         </div>
 
                     </div>
+                    <div className="flex items-center space-x-4">
+                        {stepIndex !== 0 && <button className="btn btn-small" onClick={() => removeStep(stepIndex)} onMouseEnter={() => setHoverStep(stepIndex)} onMouseLeave={() => setHoverStep(undefined)}>Delete Step</button>}
+                        {!step.subProof && <button className="btn btn-secondary btn-small" onClick={() => addSubProof(stepIndex)}>Add Subproof</button>}
+                    </div>
 
-                    {!step.subProof && <button className="btn self-end" onClick={() => addSubProof(stepIndex)}>Add SubProof</button>}
                 </li>
 
             ))}
-            <button className="btn btn-secondary" onClick={addStep}>Add Step</button>
+            <div className="flex justify-between">
+                <button className="btn btn-secondary" onClick={addStep}>Add Step</button>
+                {props.isSubProof && <button className="btn" onClick={props.onRemoveSubProof}>Delete Subproof</button>}
+            </div>
         </ol>
     )
 }
