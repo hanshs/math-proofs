b012675786aa6d24fa9c3ddc1784b5b70227bee9
NormanPirk <norman.pirk@gmail.com>
2021-12-03 21:49:14 +0200

    Cypress setup, example tests added and github action for running cypress tests (#53)

---
+++ cypress/integration/_app.spec.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+describe("rendering the app", () => {
+    beforeEach(() => {
+        cy.visit("/");
+    })
+
+    it("renders correctly", () => {
+        cy.get('#__next').should("exist");
+        cy.get('a[href*="signin"]').should("exist");
+        cy.get("#search-field").should("exist");
+        cy.get(".w-full").should("exist");
+    });
+
+    it("navigates to sign-in page", () => {
+        cy.get('a[href*="signin"]').click();
+        cy.get(".signin").should("exist");
+    });
+
+    it("naviagtes to single theorem page", () => {
+        cy.get('a[href*="theorem"]').first().click();
+        cy.get('.text-xl').should("exist");
+    });
+
+    it("filters theorems accoring to entered search phrase", () => {
+        const phrase = "Theorem statement";
+        cy.get("#search-field").type(phrase);
+        cy.get('a[href*="theorem"]')
+            .should("have.length", 1)
+            .first()
+            .should("have.text", "Example theorem statement");
+    });
+});
